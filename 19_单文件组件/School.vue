1a8fc386cd986b26d5b240d27104d11065c3bf0e
Ice <1944416694@qq.com>
2022-02-17 17:36:48 +0800

    basic vue

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="demo">
+    <h2>学生名字：{{ studentName }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    <button @click="showName">点我提示学校</button>
+  </div>
+  <!-- 组件的结构 -->
+</template>
+
+<script>
+const school = Vue.extend({
+  data() {
+    return {
+      schoolName: "gdufs",
+      address: "guangdong",
+    };
+  },
+  methods: {
+    showName() {
+      alet(this.schoolName);
+    },
+  },
+});
+// 组件交互相关的代码（数据、方法等）
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+/* 样式 */
+</style>
